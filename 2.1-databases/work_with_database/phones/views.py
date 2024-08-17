57c8fc5c7f6f6fb01a75024137b843e0e18d0162
Artem Ksenofontov <tema339517@gmail.com>
2024-08-17 23:45:27 +0300

    Выполнено третье задание по Django исправленно

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -8,8 +8,20 @@ def index(request):
 def show_catalog(request):
     phones = Phone.objects.all()
     template = 'catalog.html'
+    sort_by = request.GET.get('sort')
+
+    if sort_by == 'name':
+        phones = Phone.objects.all().order_by('name')
+    elif sort_by == 'min_price':
+        phones = Phone.objects.all().order_by('price')
+    elif sort_by == 'max_price':
+        phones = Phone.objects.all().order_by('-price')
+    else:
+        phones = Phone.objects.all()
+
     context = {
-        'phones': phones
+        'phones': phones,
+        'sort_by': sort_by
     }
     return render(request, template, context)
 
