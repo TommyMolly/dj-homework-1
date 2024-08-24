f1ed24348c4120db36667da065072f535ebb7281
Artem Ksenofontov <tema339517@gmail.com>
2024-08-24 13:42:17 +0300

    Выполненно четвертое задание по Django tags

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
+from .models import Article, Scope, ArticleScope
 
-from .models import Article
+class ArticleScopeInline(admin.TabularInline):
+    model = ArticleScope
+    extra = 1
 
-
-@admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ArticleScopeInline]
+
+    def save_related(self, request, form, formsets, change):
+        super().save_related(request, form, formsets, change)
+        obj = form.instance
+        main_scopes = obj.article_scopes.filter(is_main=True)
+        if main_scopes.count() != 1:
+            raise ValueError('У статьи должен быть один и только один основной разделю')
+
+admin.site.register(Scope)
+admin.site.register(Article, ArticleAdmin)
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,12 +1,20 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+class Scope(models.Model):
+    name = models.CharField(max_length=256,verbose_name='Название раздела')
 
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+    def __str__(self):
+        return self.name
 
 class Article(models.Model):
-
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    scopes = models.ManyToManyField(Scope, through='ArticleScope', verbose_name='Разделы')
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +22,28 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_scopes(self):
+        return self.article_scopes.select_related('tag')
+
+    def clean(self):
+        main_scopes = self.article_scopes.filter(is_main=True)
+        if main_scopes.count() > 1:
+            raise ValidationError('У статьи может быть только один основной раздел.')
+
+class ArticleScope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='article_scopes')
+    tag = models.ForeignKey(Scope, on_delete=models.CASCADE)
+    is_main = models.BooleanField(default=False, verbose_name='Основной раздел')
+
+    class Meta:
+        unique_together = ('article', 'tag')
+        verbose_name = 'Связь статьи с разделом'
+        verbose_name_plural = 'Связи статьи с разделами'
+
+    def __str__(self):
+        return f'{self.article.title} - {self.tag.name} ({"Основной" if self.is_main else "Второстепенный"})'
+
+    def clean(self):
+        if self.is_main and self.article.article_scopes.filter(is_main=True).exclude(id=self.id).exists():
+            raise ValidationError("У статьи может быть только один основной раздел.")
