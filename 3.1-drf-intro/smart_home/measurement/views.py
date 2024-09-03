e9cebb469bede18a95656ec982888136052b6f9d
Artem Ksenofontov <tema339517@gmail.com>
2024-09-03 22:45:02 +0300

    Выполнено задание по Django smart home

--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,31 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from .models import Sensor, Measurement
+from .serializers import SensorSerializer, SensorDetailSerializer, MeasurementSerializer
+
+class SensorListCreateView(ListCreateAPIView):
+    """
+    Представление для создания нового датчика и получения списка всех датчиков.
+    """
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return SensorDetailSerializer
+        return SensorSerializer
+
+
+class SensorRetrieveUpdateView(RetrieveUpdateAPIView):
+    """
+    Представление для просмотра деталей и обновления существующего датчика.
+    """
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+
+class MeasurementCreateView(CreateAPIView):
+    """
+    Представление для создания нового измерения температуры.
+    """
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
